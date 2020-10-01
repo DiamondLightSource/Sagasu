8871f20f78637a178abc11ccfeb5705cfa9851f4
co2e14 <christian.orr@diamond.ac.uk>
2020-10-01 15:02:05 +0100

    01102020_4

--- sagasu_core.py
+++ sagasu_core.py
@@ -70,7 +70,7 @@ def shelx_write(projname):
 
 def replace(file, pattern, subst):
     file_handle = open(file, "r")
-    file_string = file_handle.read()mhhh
+    file_string = file_handle.read()
     file_handle.close()
     file_string = re.sub(pattern, subst, file_string)
     file_handle = open(file, "w")
