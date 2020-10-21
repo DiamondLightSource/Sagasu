09e7e0aac99e7706fa42944182eb0982d64c72f2
co2e14 <christian.orr@diamond.ac.uk>
2020-10-21 10:14:50 +0100

    update

---
+++ phenix_emma.sh
@@ -0,0 +1,2 @@
+module load phenix 
+phenix.emma /dls/science/users/vwg85559/Sagasu/wzx/46/11/wzx_fa.pdb /dls/science/users/vwg85559/Sagasu/wzx/47/12/wzx_fa.pdb --tolerance=6 --space_group=C2 2>&1 | tee -a sagasu.log
---
+++ wait.sh
@@ -0,0 +1,2 @@
+module load python/3 
+/home/i23user/bin/shelxgrid/sagasu_analysis 2>&1 | tee -a sagasu.log
